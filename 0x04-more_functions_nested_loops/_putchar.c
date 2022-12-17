02cc1b0db629210f9f09873fa9c6d4a022a34ef7
“grace” <gracenanaamamartin@gmail.com>
2022-12-16 18:34:33 -0800

    task 0

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -3,23 +3,23 @@
 
 
 /**
+
+ * _putchar - writes the character c to stdout
+
+ * @c: The character to print
+
  *
- *  * _putchar - writes the character c to stdout
- *
- *   * @c: The character to print
- *
- *    *
- *
- *     * Return: On success 1.
- *
- *      * On error, -1 is returned, and errno is set appropriately.
- *
- *       */
+
+ * Return: On success 1.
+
+ * On error, -1 is returned, and errno is set appropriately.
+
+ */
 
 int _putchar(char c)
 
 {
 
-		return (write(1, &c, 1));
+	return (write(1, &c, 1));
 
 }
