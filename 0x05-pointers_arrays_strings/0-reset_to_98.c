fb9675a86b48c075e285d752b7377b4aea67b451
“grace” <gracenanaamamartin@gmail.com>
2022-12-19 22:09:41 -0800

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * reset_to_98 - updates the value it points to to 98
+ * reset_to_98 - updates a integer value to 98
  * @n: A pointer to an int that will be updated
- * Return:: void that means is correct
+ * Return: void that means is correct
  */
+
 
 void reset_to_98(int *n)
 
 {
 *n = 98;
 }
+
+
