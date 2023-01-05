93568112321133a351244bf0bff5f4713961390c
gracE005 <gracenanaamamartin@gmail.com>
2023-01-05 13:02:29 -0800

    task2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,23 +1,18 @@
-/*
- * File: 2-strlen_recursion.c
- * Auth: smone
- */
-
 #include "main.h"
 
 /**
- * _strlen_recursion - Returns the length of a string.
- * @s: The string to be measured.
- * Return: The length of the string.
+ *_strlen_recursion - Returns the length of a string.
+ *@s: The string to be measured.
+ *Return: The length of the string.
  */
-int_strlen_recursion(char *s)
-{
-int len = 0;
 
+int _strlen_recursion(char *s)
+{
+int longit = 0;
 if (*s)
 {
-len++;
-len + = _strlen_recursion(s + 1);
+longit++;
+longit += _strlen_recursion(s + 1);
 }
-return (len);
+return (longit);
 }
