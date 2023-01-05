c79e5c78fcb7b9ee7f955df598429f30310a32e7
gracE005 <gracenanaamamartin@gmail.com>
2023-01-05 13:20:34 -0800

    task101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,6 +8,7 @@
  */
 
 int wildcmp(char *s1, char *s2)
+
 {
 if (*s1 == '\0')
 {
@@ -21,10 +22,9 @@ if (*s2 == '*')
 {
 return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 }
-else
-if (*s1 == *s2)
-}
+else if (*s1 == *s2)
+{
 return (wildcmp(s1 + 1, s2 + 1));
-}
+{
 return (0);
 }
