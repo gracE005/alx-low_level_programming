ee551a82aeab2741c4da834afd17afd59e2fc1c5
gracE005 <gracenanaamamartin@gmail.com>
2023-01-09 11:05:03 -0800

    task0

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -6,7 +6,7 @@
  * @n: number of bytes to be changed
  * Return: changed array with new value for n bytes
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
 int i = 0;
 
--- 0x09-static_libraries/0-strcat.c
+++ 0x09-static_libraries/0-strcat.c
@@ -5,7 +5,7 @@
  * @src: input value
  * Return: void
  */
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
 int i;
 int j;
