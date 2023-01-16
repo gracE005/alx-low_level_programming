84b44b4f548fcee9ad031512eb30c0e6e1078cc0
gracE005 <gracenanaamamartin@gmail.com>
2023-01-16 11:10:19 -0800

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,11 +2,10 @@
 
 /**
  * main - prints the name of the file
- *
  * Return: Always 0 (Success)
  */
 int ain(void);
 {
-	printf("%s\n", __FILE__);
-	return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
