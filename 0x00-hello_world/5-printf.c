02882bb7246e0243ef6f210f159c25102438a317
gracE005 <gracenanaamamartin@gmail.com>
2023-01-18 10:55:22 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  *main - Entry point
  *My hello, printf task
  *Return: return 1 after ptogram execution
  */
+
 int main(void)
 {
 printf("with proper grammer, but the outcome is a piece of art,\n");
