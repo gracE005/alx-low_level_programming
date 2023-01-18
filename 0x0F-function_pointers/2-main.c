81df4db8f0dbf58a4098014f68cf349a0f97fa7f
gracE005 <gracenanaamamartin@gmail.com>
2023-01-18 09:58:53 -0800

    2-main.c

---
+++ 0x0F-function_pointers/2-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * is_98 - check if a number is equal to 98
+ * @elem: the integer to check
+ * Return: 0 if false, something else otherwise.
+ */
+int is_98(int elem)
+{
+return (elem == 98);
+}
+
+/**
+ * is_strictly_positive - check if a number is greater than 0
+ * @elem: the integer to check
+ *
+ * Return: 0 if false, something else otherwise.
+ */
+int is_strictly_positive(int elem)
+{
+return (elem > 0);
+}
