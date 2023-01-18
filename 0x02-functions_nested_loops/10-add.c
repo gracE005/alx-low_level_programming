2b42c59e24d41e75ba2f9db91ef7532fcfafe070
gracE005 <gracenanaamamartin@gmail.com>
2023-01-18 14:20:04 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,19 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * add - adds two integers and returns the result
- * @a: int to be added to b
- * @b: int to be added to a
- * Return: sum of a and b
+ * add -prints add two numbers
+ *@i: print int i
+ *@k: print int k
+ * Return: Always 0.
  */
 
-int add(int a, int b);
+int add(int i, int k)
 {
-int c;
-
-
-c = a + b;
-
-
-return (c);
+return (i + k);
 }
