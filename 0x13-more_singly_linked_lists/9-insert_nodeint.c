aa382b8ab4c6651dc9bf0235b7967b81b84a2966
gracE005 <gracenanaamamartin@gmail.com>
2023-01-31 15:34:43 -0800

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -6,7 +6,6 @@
  * @head: pointer to the first node in the list
  * @idx: index where the new node is added
  * @n: data to insert in the new node
- *
  * Return: pointer to the new node, or NULL
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
@@ -32,7 +31,7 @@ return (new);
 for (i = 0; temp && i < idx; i++)
 {
 if (i == idx - 1)
-{
+{        
 new->next = temp->next;
 temp->next = new;
 return (new);
@@ -40,6 +39,5 @@ return (new);
 else
 temp = temp->next;
 }
-
 return (NULL);
 }
