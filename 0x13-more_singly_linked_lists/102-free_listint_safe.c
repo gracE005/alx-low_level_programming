39fdbb05d48222571ee4fd2c3f3c3ed5571e8b68
gracE005 <gracenanaamamartin@gmail.com>
2023-01-31 15:13:55 -0800

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -3,7 +3,6 @@
 /**
  * free_listint_safe - frees a linked list
  * @h: pointer to the first node in the linked list
- *
  * Return: number of elements in the freed list
  */
 size_t free_listint_safe(listint_t **h)
